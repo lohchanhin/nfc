f619ad25179514bcd571512207a82c11f73f7472
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-07-13 23:34:52 -0700

    refactor layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <header class="p-4 bg-gray-100 flex gap-4">
-      <NuxtLink to="/" class="hover:underline">Home</NuxtLink>
-      <NuxtLink to="/login" class="hover:underline">Login</NuxtLink>
-      <NuxtLink to="/register" class="hover:underline">Register</NuxtLink>
-      <NuxtLink to="/dashboard" class="hover:underline">Dashboard</NuxtLink>
-    </header>
+  <div class="flex">
+    <aside class="p-4 bg-gray-100 flex flex-col gap-4">
+      <NuxtLink to="/" class="hover:underline">首頁</NuxtLink>
+      <NuxtLink to="/login" class="hover:underline">登入</NuxtLink>
+      <NuxtLink to="/register" class="hover:underline">註冊</NuxtLink>
+      <NuxtLink to="/dashboard" class="hover:underline">儀表板</NuxtLink>
+    </aside>
     <main>
       <slot />
     </main>
